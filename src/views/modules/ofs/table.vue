0de3579f3f9ec7344184b36769cd5bd6347c6432
qianmua <49187025+qianmua@users.noreply.github.com>
2020-10-08 22:08:55 +0800

    xsdhmy

---
+++ src/views/modules/ofs/table.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <el-form :model="numberValidateForm" ref="numberValidateForm" label-width="100px" class="demo-ruleForm">
+            <el-form-item
+                label="日期"
+                prop="age"
+            >
+                <el-date-picker
+                v-model="value1"
+                type="date"
+                placeholder="选择日期">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('numberValidateForm')">提交</el-button>
+                <el-button @click="resetForm('numberValidateForm')">重置</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        numberValidateForm: {
+          age: ''
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
